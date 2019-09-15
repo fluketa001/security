89a069915dfb90eb859e1b909952da97beeee535
fluketa001 <fluketa010@hotmail.com>
2019-09-16 00:59:22 +0700

    template sidebar

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.app')
 
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
